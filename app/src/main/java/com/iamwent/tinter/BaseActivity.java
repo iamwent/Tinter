c7bb176e93c9abffae20f1d5ce142fc724e67946
iamwent <ilovewent@gmail.com>
2016-08-27 10:30:50 +0800

    use style to fit system window

--- app/src/main/java/com/iamwent/tinter/BaseActivity.java
+++ app/src/main/java/com/iamwent/tinter/BaseActivity.java
@@ -26,7 +26,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(provideContentViewId());
-        setTranslucentStatusBar();
+//        setTranslucentStatusBar();
 
     }
 
